30800394f0292e033fbc245321c50945376c012a
Manoj Guniganti <manojkumarg837@gmail.com>
2023-12-09 17:48:10 +0530

    true false conditions

--- addition1.js
+++ addition1.js
@@ -1,4 +1,65 @@
-var variableOne = 4;
-var variableTwo = 5;
-var variableThree = variableOne + variableTwo ;
-console.log(variableThree);
+variableOne = 12;
+
+variableTwo = 16;
+
+variableOne>variableTwo;  //false
+
+console.log(variableOne>variableTwo);
+
+variableOne<variableTwo; //true
+
+console.log(variableOne<variableTwo);
+
+var variablebulb = {
+
+    company :"bajaj",
+
+    color : "white",
+
+}
+
+console.log(variablebulb);
+
+var variablecompanyofbulb = "bajaj";
+
+var variablecolor = "white";
+
+
+var variablecompanyofbulb = "bajaj";
+
+console.log("variablecompanyofbulb ,variablecolor");
+
+var companyofbulb = "bajaj";
+
+var color = "white";
+
+
+console.log("companyofbulb ,color");
+
+var companyOfNotebook = "classmate";
+
+var pages = 180;
+
+console.log(companyOfNotebook,pages);
+
+var variableshirt = {
+
+    brandname : 'Denim',
+
+    size : 32,
+
+    color : "black",
+
+}
+
+console.log(variableshirt);
+
+
+
+var name = "pavan"
+
+var quotationOne = "shift the gear and disappear"
+
+console.log(name);
+
+console.log(quotationOne);
---
+++ condition.js
@@ -0,0 +1,12 @@
+var variableOne = 10;
+var variableTwo = 20;
+var variableThree = (variableTwo < variableOne); //false
+var variableFour = (variableOne < variableTwo); //true
+if (variableOne < variableTwo)
+{
+console.log("true")
+}    
+else(variableTwo < variableOne)
+{
+console.log("false")
+}
