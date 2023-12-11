e8c363a0ef8c58237c37d664d6d7420a0914e1e1
Manoj Guniganti <manojkumarg837@gmail.com>
2023-12-11 17:16:14 +0530

    summ.js

---
+++ summ.js
@@ -0,0 +1,17 @@
+/*
+Q) write a program to print  sum of first five numbers ?
+out put :
+15
+ */
+var i,sum=0 ;
+for (var i= 1 ; i<=5 ;i++) {
+ var sum=sum+i;
+    console.log(sum);
+}
+  
+
+var sum=0;
+for (var i= 1 ; i<=5 ;i++) {
+    sum+=i;
+}
+ console.log("Sum of numbers for 1 to 5:",sum);
